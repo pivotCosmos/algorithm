14c03ec9d8debbc309275388f0400ba1d38938f7
adeemgin <99481661+pivotCosmos@users.noreply.github.com>
2022-06-04 08:47:51 +0900

    list comprehension 사용

--- ex0604/4673_selfNumber.py
+++ ex0604/4673_selfNumber.py
@@ -2,14 +2,13 @@
 
 originalList = [x for x in range(1,10001)] #list comprehension
 
-#newList = [x for x in originalList if x+ in originalList]
-
-tempStrList = []
-newList = []
+tempStrList = [] #분리된 숫자 저장
+newList = [] #셀프넘버가 아닌 숫자 저장
 for i in range(len(originalList)) :
     tempStrList = list(map(int,str(originalList[i])))
     newNum = originalList[i] + sum(tempStrList)
     newList.append(newNum)
 
-resultList = list(set(originalList)-set(newList))
+#resultList = list(set(originalList)-set(newList)) #셀프넘버 저장
+resultList = [x for x in originalList if x not in newList] #list comprehension
 print(*sorted(resultList), sep="\n")
