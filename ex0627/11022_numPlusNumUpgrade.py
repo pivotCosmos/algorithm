570fa5444196d06d93d86720ff836c3bf2e356b1
adeemgin <99481661+pivotCosmos@users.noreply.github.com>
2022-06-27 10:28:50 +0900

    반복문

---
+++ ex0627/11022_numPlusNumUpgrade.py
@@ -0,0 +1,6 @@
+import sys
+
+n = int(input())
+for i in range (1,n+1) :
+    num1,num2 = map(int, sys.stdin.readline().split())
+    print(f'Case #{i}: {num1} + {num2} = {num1+num2}')
---
+++ ex0627/2438_printStars.py
@@ -0,0 +1,8 @@
+n = int(input())
+# for i in range (n) :
+#     stars = ''
+#     for j in range(i+1) :
+#         stars = stars + '*'
+#     print(stars)
+for i in range (1, n+1) :
+    print('*'*i)
