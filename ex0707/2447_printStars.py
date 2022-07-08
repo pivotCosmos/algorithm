987f445c8dba6244d9eb08b0db42bd926f1a397e
adeemgin <99481661+pivotCosmos@users.noreply.github.com>
2022-07-08 22:54:53 +0900

    별찍기 수정

--- ex0707/2447_printStars.py
+++ ex0707/2447_printStars.py
@@ -1,8 +1,20 @@
-def printStars (n) :
+def printStars (l,n) :
+    if n == l :
+        return
+    elif l == 0 :
+        print('***'*n)
+        print('* *'*n)
+        print('***'*n)
+        printStars(l+1, n)
+    elif l == 1 :
+        print('***   ***')
+        print('* *   ***')
+        print('***   ***')
+        printStars(l+1, n)
+    elif l == 2 :
+        print('***'*n)
+        print('* *'*n)
+        print('***'*n)
+        printStars(l+1, n)
 
-    print('***')
-    print('* *')
-    print('***')
-    return
-
-printStars(int(input()))
+printStars(0,int(input()))
