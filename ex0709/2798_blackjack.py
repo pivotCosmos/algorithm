afceb6ff9c7e9a2e8d69b9adc4ed36dbed8137a6
adeemgin <99481661+pivotCosmos@users.noreply.github.com>
2022-07-12 22:01:25 +0900

    브루트포스 수정

--- ex0709/2798_blackjack.py
+++ ex0709/2798_blackjack.py
@@ -1,19 +1,17 @@
 # 브루트 포스
 n,m = map(int,input().split())
 li = list(map(int, input().split()))
-temp = []
+re = 0
 for i in range(n) :
     for j in range(n) :
         for k in range(n) :
             if i != j and j != k and i != k :
-                temp.append([li[i],li[j],li[k]])
-sums = []
-for i in range(len(temp)) :
-    s = sum(temp[i])
-    if s <= m :
-        sums.append(sum(temp[i]))
+                temp = [li[i],li[j],li[k]]
+                sums = sum(temp)
+                if sums >= re and sums <= m:
+                    re = sums
 
-print(max(sums))
+print(re)
 
 # from itertools import combinations 
 
