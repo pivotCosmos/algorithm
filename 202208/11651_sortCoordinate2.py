43cbdaa107bd1afc0c56ce295bed11c3defc5600
jeongah <adeemgin@naver.com>
2022-08-05 15:28:10 +0900

    출력 수정

--- 202208/11651_sortCoordinate2.py
+++ 202208/11651_sortCoordinate2.py
@@ -2,5 +2,5 @@ import sys
 N = int(input())
 nums = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 sortedNums = sorted(nums, key=lambda x : (x[1],x[0]))
-for x, y in nums:
+for x, y in sortedNums:
     print(x, y)
