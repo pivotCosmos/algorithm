7832965f0e90de8bb97a42a9896929f5d000a6d5
adeemgin <99481661+pivotCosmos@users.noreply.github.com>
2022-08-14 23:54:57 +0900

    수정 - 람다 대신 함수로 풀기

--- 202208/3009_fourthPoint.py
+++ 202208/3009_fourthPoint.py
@@ -1,7 +1,19 @@
+from re import X
+
+
 xlist = []
 ylist = []
 for i in range(3) :
     a,b = map(int,input().split())
     xlist.append(a)
     ylist.append(b)
-print(min(xlist, key=lambda x : xlist.count(x)), min(ylist, key=lambda x: ylist.count(x)))
+
+def findMinCount(array) :
+    counts = []
+    for i in range(len(array)) :
+        count = array.count(array[i])
+        counts.append(count)
+    index = counts.index(min(counts))
+    return array[index]
+
+print(findMinCount(xlist), findMinCount(ylist))
