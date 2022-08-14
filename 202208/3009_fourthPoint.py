2e6164c12256a560e2d0734637d1d0d2d6171d36
adeemgin <99481661+pivotCosmos@users.noreply.github.com>
2022-08-14 23:16:51 +0900

    수정 - min(list, key=lambda) 사용

--- 202208/3009_fourthPoint.py
+++ 202208/3009_fourthPoint.py
@@ -1,19 +1,7 @@
-import sys
-points = [list(map(int, sys.stdin.readline().split())) for i in range(3)]
-result = []
-
-points.sort()
-
-if points[0][0] != points[1][0] :
-    result.append(points[0][0])
-elif points[-1][0] != points[-2][0] :
-    result.append(points[-1][0])
-
-points.sort(key=lambda x : x[1])
-
-if points[0][1] != points[1][1] :
-    result.append(points[0][1])
-elif points[-1][1] != points[-2][1] :
-    result.append(points[-1][1])
-
-print(*result)
+xlist = []
+ylist = []
+for i in range(3) :
+    a,b = map(int,input().split())
+    xlist.append(a)
+    ylist.append(b)
+print(min(xlist, key=lambda x : xlist.count(x)), min(ylist, key=lambda x: ylist.count(x)))
