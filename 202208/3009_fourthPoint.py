020239bf47dbde9b9150d52ccd10e4fe348e215b
adeemgin <99481661+pivotCosmos@users.noreply.github.com>
2022-08-15 00:06:57 +0900

    수정 - 함수와 람다 비교

--- 202208/3009_fourthPoint.py
+++ 202208/3009_fourthPoint.py
@@ -5,12 +5,12 @@ for i in range(3) :
     xlist.append(a)
     ylist.append(b)
 
-def findMinCount(array) :
-    counts = []
-    for i in range(len(array)) :
-        count = array.count(array[i])
-        counts.append(count)
-    index = counts.index(min(counts))
-    return array[index]
+# def findMinCount(array) :
+#     counts = []
+#     for i in range(len(array)) :
+#         count = array.count(array[i])
+#         counts.append(count)
+#     index = counts.index(min(counts))
+#     return array[index]
 
-print(findMinCount(xlist), findMinCount(ylist))
+print(min(xlist, key=lambda x: xlist.count(x)), min(ylist, key=lambda x: ylist.count(x)))
