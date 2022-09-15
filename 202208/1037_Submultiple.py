68533a9bdf6a383ba9969c926671c1181c188c46
adeemgin <99481661+pivotCosmos@users.noreply.github.com>
2022-09-16 00:03:11 +0900

    [수정] 누락된 예약어 추가

--- 202208/1037_Submultiple.py
+++ 202208/1037_Submultiple.py
@@ -2,12 +2,10 @@ import sys
 
 N = int(input())
 submultiple_list = list(map(int, sys.stdin.readline().split()))
+
 if N == 1 :
-    unique_submultiple = submultiple_list[0]
-    result = unique_submultiple**2
+    result = submultiple_list[0]**2
     print(result)
 else :
-    min_num = min(submultiple_list)
-    max_num = max(submultiple_list)
-    result = min_num*max_num
+    result = min(submultiple_list)*max(submultiple_list)
     print(result)
