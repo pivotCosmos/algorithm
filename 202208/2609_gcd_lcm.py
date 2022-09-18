591bb9ff34e54773cf466361e30544895f4af4bd
adeemgin <99481661+pivotCosmos@users.noreply.github.com>
2022-09-19 00:27:30 +0900

    [수정] 입력값 대소비교 def로 분리

--- 202208/2609_gcd_lcm.py
+++ 202208/2609_gcd_lcm.py
@@ -1,6 +1,12 @@
 # 최대공약수, 최소공배수 구하기
 N, M = map(int, input().split())
 
+def sort_inputs_asc(N, M) :
+    small_num = N if N < M else M
+    big_num = N if N > M else M
+    results = [small_num, big_num]
+    return results
+
 def get_submultiples(number) :
     submultiples = []
     for i in range(2, int(number/2)) :
@@ -11,10 +17,9 @@ def get_submultiples(number) :
 
 def get_GCD(num1, num2) :
     GCD = 0
-    input_numbers = [num1, num2]
-    input_numbers.sort()
-    small_submultiples = get_submultiples(input_numbers[0])
-    big_submultiples = get_submultiples(input_numbers[1])
+    nums = sort_inputs_asc(num1, num2)
+    small_submultiples = get_submultiples(nums[0])
+    big_submultiples = get_submultiples(nums[1])
 
     for big_submultiple in big_submultiples :
         if big_submultiple in small_submultiples :
