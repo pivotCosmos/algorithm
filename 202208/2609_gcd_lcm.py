dff8884194a0cd039b0ccea59983506e21be7f13
adeemgin <99481661+pivotCosmos@users.noreply.github.com>
2022-09-18 23:50:11 +0900

    [수정] 최대공약수 구하기

--- 202208/2609_gcd_lcm.py
+++ 202208/2609_gcd_lcm.py
@@ -1,29 +1,29 @@
 # 최대공약수, 최소공배수 구하기
 N, M = map(int, input().split())
 
-GCD = 0
-LCM = 0
-
 def get_submultiples(number) :
     submultiples_of_number = []
-    for i in range(number/2) :
+    for i in range(2, int(number/2)) :
         if number%i == 0 :
             submultiples_of_number.append(i)
     submultiples_of_number.sort(reverse=True)
     return submultiples_of_number
 
-submultiples_of_N = get_submultiples(N)
-submultiples_of_M = get_submultiples(M)
+def get_GCD(num1, num2) :
+    GCD = 0
+    submultiples_num1 = get_submultiples(num1)
+    submultiples_num2 = get_submultiples(num2)
+    smaller_list = []
 
-smaller_list = []
+    if N < M :
+        smaller_list = submultiples_num1
+    else :
+        smaller_list = submultiples_num2
 
-if N < M :
-    smaller_list = submultiples_of_N
-else :
-    smaller_list = submultiples_of_M
+    for s in smaller_list :
+        if s in smaller_list :
+            GCD = s
+            break
+    return GCD
 
-for s in smaller_list :
-    if s in smaller_list :
-        GCD = s
-        break
-
+print(get_GCD(N,M))
