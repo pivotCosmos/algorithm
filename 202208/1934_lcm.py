f7235637a76a985b808346d5061cb57c18211bd6
adeemgin <99481661+pivotCosmos@users.noreply.github.com>
2022-09-21 00:02:21 +0900

    [수정] 변수 수정, 반복문 추가

--- 202208/1934_lcm.py
+++ 202208/1934_lcm.py
@@ -1,13 +1,16 @@
 # 최소공배수
+import sys
 
-A, B = map(int, input().split())
+N = int(input())
+for i in range(N) :
+    A, B = map(int, input().split())
 
-n1 = A
-n2 = B
+    n1 = A
+    n2 = B
 
-while n2 != 0 :
-    temp = n1
-    n1 = n2
-    n2 = temp%n1
+    while n2 != 0 :
+        temp = n1
+        n1 = n2
+        n2 = temp%n1
 
-print(A*B//n2)
+    print(A*B//n1)
