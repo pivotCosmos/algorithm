8113c2bff6d1277ac8fc5eb728c56b85b8e40427
adeemgin <99481661+pivotCosmos@users.noreply.github.com>
2022-09-27 23:39:50 +0900

    [수정] 나이순 정렬 - str -> int로 변환 후 정렬

--- 202208/10814_sortAge.py
+++ 202208/10814_sortAge.py
@@ -1,6 +1,8 @@
 import sys
 N = int(input())
-members = [list(sys.stdin.readline().rstrip().split()) for _ in range(N)]
+members = [list(sys.stdin.readline().split()) for _ in range(N)]
+for member in members :
+    member[0] = int(member[0])
 newMembers = sorted(members, key=lambda x: x[0])
 for member in newMembers :
-    print(*member, sep=' ')
+    print(*member)
