1bacab0e08d907cd11ea0313c75838c2233372b1
adeemgin <99481661+pivotCosmos@users.noreply.github.com>
2022-09-28 23:50:45 +0900

    [시간초과] 골드바흐 추측

--- 202207/ex0702/9020_gold.py
+++ 202207/ex0702/9020_gold.py
@@ -1,28 +1,26 @@
-from itertools import *
+import sys
 
 # 에라토스테네스의 체
-sieve = [x for x in range(2, 10000)]
-for i in range(2, int(10000*0.5)) :
-    for j in range(i*2, 10000, i) :
+sieve = [x for x in range(2, 10001)]
+for i in range(2, int(10000*0.5)+1) :
+    for j in range(i*2, 10001, i) :
         if j in sieve :
             sieve.remove(j)
 
 def find(x) :
-    results = []
-    sums = []
+    one = 0; two = 10001
     for i in range(len(sieve)) :
-        while x < sieve[i] :
-            for j in range(len(sieve)) :
-                while x < sieve[j] :
-                    if sieve[i] + sieve[j] == x :
-                        results.append([sieve[i], sieve[j]])
-                        sums.append(sieve[i]+sieve[j])
-    temp = min(sums)
-    return results[temp]
+        v = x - sieve[i]
+        if v in sieve :
+            if v >= sieve[i] : # one <= two
+                one = sieve[i]
+                two = v
+            else :
+                break
+    return one, two
 
-T = int(input())
-results = []
-for i in range(T) :
-    n = int(input())
+T = int(sys.stdin.readline())
+for _ in range(T) :
+    n = int(sys.stdin.readline())
     answer = find(n)
     print(*answer)
