76fc3e428b3926384639133011ae071521830b89
adeemgin <99481661+pivotCosmos@users.noreply.github.com>
2022-09-29 23:59:21 +0900

    [생성] 수 정렬3 - 카운팅 정렬

---
+++ 202209/10989_sort_num_ver3.py
@@ -0,0 +1,11 @@
+import sys
+N = int(sys.stdin.readline())
+# numbers = [int(sys.stdin.readline()) for _ in range(N)] # 최대 10,000,000개라서 저장하면 메모리 초과
+counts = [0]*10000
+for _ in range(N) :
+    v = int(sys.stdin.readline())
+    counts[v] += 1
+
+for index in range(len(counts)) :
+    for v in range(counts[index]) :
+        print(index)
