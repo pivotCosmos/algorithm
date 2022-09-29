5c40cf31aa6278eff6a211fe7593e03d1ca3213e
adeemgin <99481661+pivotCosmos@users.noreply.github.com>
2022-09-30 00:03:43 +0900

    [수정] 입력값 최대 개수만큼 인덱스 수정

--- 202209/10989_sort_num_ver3.py
+++ 202209/10989_sort_num_ver3.py
@@ -1,7 +1,7 @@
 import sys
 N = int(sys.stdin.readline())
 # numbers = [int(sys.stdin.readline()) for _ in range(N)] # 최대 10,000,000개라서 저장하면 메모리 초과
-counts = [0]*10000
+counts = [0]*10001
 for _ in range(N) :
     v = int(sys.stdin.readline())
     counts[v] += 1
