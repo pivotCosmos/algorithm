20f0f9d410a4570fbfd7335a1f4c7c2a50379ce1
adeemgin <99481661+pivotCosmos@users.noreply.github.com>
2022-10-11 23:54:24 +0900

    [생성] 이항 계수1

---
+++ 202210/11050_binomialCoefficient.py
@@ -0,0 +1,24 @@
+# 이항계수
+import sys
+N, K = map(int, sys.stdin.readline().split())
+
+def factorial(num) :
+    oriNum = num
+    result = oriNum
+    while oriNum-1 > 1 :
+        result *= (oriNum-1)
+        oriNum -= 1
+    return result
+
+# N개 중에서 K개를 뽑는다
+def getBinomicalCoefficient(totalNum, pickNum) :
+    result = 1 # K=0일때
+    temp1 = factorial(totalNum)
+    temp2 = factorial(pickNum)
+    temp3 = factorial(totalNum-pickNum)
+    if temp2 > 0 and temp3 > 0 :
+        result = temp1//(temp2*temp3)
+    return result
+
+result = getBinomicalCoefficient(N,K)
+print(result)
