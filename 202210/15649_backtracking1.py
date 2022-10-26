82c684dc62bd2a9316aac0377b9ac10d800a9f01
adeemgin <99481661+pivotCosmos@users.noreply.github.com>
2022-10-26 22:25:21 +0900

    [수정] 백트래킹1

--- 202210/15649_backtracking1.py
+++ 202210/15649_backtracking1.py
@@ -1,11 +1,19 @@
 N,M = map(int, input().split())
 original_li = [x for x in range(1,N+1)]
-# 두개 뽑는다고 가정
-for i in original_li :
-    copy_list = original_li.copy() # 깊은 복사
-    one = i
-    copy_list.remove(one)
-    for j in copy_list :
-        result_li = [one]
-        result_li.append(j)
+result_li = []
+
+def get_new_result_li(copy_li, result_li, total_pick) :
+    if total_pick > 0 :
+        for i in copy_li :
+            new_result_li = result_li.copy()
+            pick = i
+            new_result_li.append(pick)
+
+            new_copy_li = copy_li.copy()
+            new_copy_li.remove(pick)
+            get_new_result_li(new_copy_li,new_result_li,total_pick-1)
+    else :
         print(*result_li)
+    return 0
+
+get_new_result_li(original_li, result_li, M)
