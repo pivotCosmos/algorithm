63c0508b00f8e92e7315704395b125ee91c793c9
adeemgin <99481661+pivotCosmos@users.noreply.github.com>
2022-10-28 00:02:42 +0900

    [수정] 백트래킹2 - 괄호없이 출력

--- 202210/15650_backtracking2.py
+++ 202210/15650_backtracking2.py
@@ -2,5 +2,6 @@ from itertools import combinations
 N,M = map(int,input().split())
 li = [x for x in range(1,N+1)]
 depth = M
-results = list(combinations(li,M))
-print(*results, sep='\n')
+results = [x for x in combinations(li,M)]
+for tpl in results :
+    print(*list(tpl))
