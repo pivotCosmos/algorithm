45ce1c2e6482e18ae9e780e4df7bff1e148adbd1
adeemgin <99481661+pivotCosmos@users.noreply.github.com>
2022-11-23 00:52:09 +0900

    update 1874_stack_sequence

--- 202211/1874_stack_sequence.py
+++ 202211/1874_stack_sequence.py
@@ -1,48 +1,28 @@
+# https://www.acmicpc.net/problem/1874
 import sys
-# stack
-class stack :
-    def __init__(self) :
-        self.items = []
-    def push(self, item) :
-        self.items.append(item)
-    def pop(self) :
-        return self.items.pop()
-    def is_empty(self) :
-        return not self.items
+try :
+    N = int(input())
+    answer = [int(sys.stdin.readline()) for _ in range(N)] # determined. ex) answer = [4,3,6,8,7,5,2,1]
+    stack = [] # save numbers in asc (from 1 to N)
+    lg = [] # log. Put one to stack, save '+' in li. Pop one from stack, save '-' in li.
 
-N = int(input())
-# setting
-my_stack = stack()
-my_list = []
-for _ in range(N) :
-    num = int(sys.stdin.readline())
-    my_stack.push(num)
-    my_list.append(num)
-now_value = 0
-count = 0 # 오름차순 수열의 수 개수
-result_li = []
-# check if the numbers are descending after the max value
-max_index = my_list.index(N)
-my_list_sliced = my_list[max_index:len(my_list)]
-my_list_sliced_dsc = sorted(my_list_sliced,reverse=True)
-if my_list_sliced != my_list_sliced_dsc :
-    print('NO')
-else :
-    #print
-    while not my_stack.is_empty() :
-        now_value += 1
-        stack_value = my_stack.pop()
-        if now_value == stack_value :
-            result_li.append('+')
-            count += 1
-        elif now_value < stack_value :
-            gap = stack_value - now_value
-            result_li += ['+']*gap
-            result_li += ['-']*gap
-            result_li.append('+')
-            now_value += gap
-            count += 1
-        else :
-            result_li += ['-']*count
-            break
-    print(*result_li,sep="\n")
+    j = 0 # answer index
+    for i in range(1,N+1) :
+        stack.append(i) # stack = [1,2,3,...,N+1]
+        lg.append('+') # log
+
+        while j < N and answer[j] == stack[-1] : # jth answer value [4,3,6,8,7,5,2,1] matches last stack input [1,2,3,...,N+1]
+            stack.pop() # pop one
+            lg.append('-') # log
+            j += 1 # next val
+            if not stack:
+                break
+    
+    if stack:
+        print('NO')
+    else :
+        for char in lg :
+            print(char)
+
+except ValueError :
+    print('Input Error')
